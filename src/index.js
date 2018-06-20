b98ca4c0523a51e19d25018a5f262496d65cb5b3
kib_keng@hotmail.com <Keng5734>
2018-06-21 01:03:29 +0700

    setup new post page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
-import promise from "redux-promise";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
-import reducers from "./reducers";
-import PostsIndex from "./components/posts_index";
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_news';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,9 +15,12 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
-  document.querySelector(".container")
+  document.querySelector('.container')
 );
