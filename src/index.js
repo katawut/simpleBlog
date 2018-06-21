5ab5e93f97bca554c13ea47d58b7d4595969ab8f
Katawut Utaipaisalwong <katawut@apples-iMac.local>
2018-06-21 18:57:18 +0700

    setup posts show

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import promise from 'redux-promise';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
-import reducers from './reducers';
-import PostsIndex from './components/posts_index';
-import PostsNew from './components/posts_news';
+import reducers from "./reducers";
+import PostsIndex from "./components/posts_index";
+import PostsNew from "./components/posts_news";
+import PostsShow from "./components/posts_show";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,11 +17,13 @@ ReactDOM.render(
     <BrowserRouter>
       <div>
         <Switch>
+          {/* route order must be corrected */}
           <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
           <Route path="/" component={PostsIndex} />
         </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
-  document.querySelector('.container')
+  document.querySelector(".container")
 );
