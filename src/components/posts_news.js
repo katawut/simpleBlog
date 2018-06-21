f57e87272779d1b4021a36b11ff0bb6d1bcacc8a
Katawut Utaipaisalwong <katawut@apples-iMac.local>
2018-06-21 17:07:48 +0700

    set form and form validate

--- src/components/posts_news.js
+++ src/components/posts_news.js
@@ -1,9 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
+  renderField(field) {
+    //const { meta } = field; // from field.meta
+    const {
+      meta: { touched, error }
+    } = field; // refactor by es6 get nest properties (field.meta.touched, field.meta.error)
+
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input} // map all event hadler to input
+        />
+        <div className="text-help">{touched ? error : ""}</div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
-    return <div>Post new!</div>;
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+
+  if (!values.categories) {
+    errors.categories = "Enter a categories!";
+  }
+
+  if (!values.content) {
+    errors.content = "Enter a content!";
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(PostsNew);
